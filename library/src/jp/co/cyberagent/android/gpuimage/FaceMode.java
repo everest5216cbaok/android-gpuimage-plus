9cbc9117c5075573f03b327cf23aff96fd8f7a28
jizhen <jizhen@baozou.com>
2015-10-20 09:55:10 +0800

    补充相关忘记提交的代码。

---
+++ library/src/jp/co/cyberagent/android/gpuimage/FaceMode.java
@@ -0,0 +1,8 @@
+package jp.co.cyberagent.android.gpuimage;
+
+public enum FaceMode {
+    //图像来自于照相机抓取
+    IMAGE_FROM_CAMERA, 
+    //图像来自于图片
+    IMAGE_FROM_PICTURE
+}
---
+++ library/src/jp/co/cyberagent/android/gpuimage/LogTag.java
@@ -0,0 +1,5 @@
+package jp.co.cyberagent.android.gpuimage;
+
+public class LogTag {
+    public static final String TEXTURENUMTAG = "textureNUMTag";
+}
